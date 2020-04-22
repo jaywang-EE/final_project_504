4ca158eaeafbdf4a9dc1361085c84bcf37aba28c
jaywang-EE <gaaiia37896@gmail.com>
2020-04-22 15:19:49 -0400

    Update cp_dataset_19.py

--- cp_dataset_19.py
+++ cp_dataset_19.py
@@ -93,7 +93,7 @@ class CPDataset(data.Dataset):
 
         #np.array(Image.open("test.png"))
         # parsing segmentation
-        parse_array = np.array(im_parse)
+        parse_array = np.asarray(im_parse)
         parse_arrays = [np.expand_dims((parse_array == i).astype(np.float32), axis=0) for i in range(16)]
         print(im_parse.size, parse_array.shape, parse_arrays[0].shape)
         parse_shape = (parse_array > 0).astype(np.float32)
