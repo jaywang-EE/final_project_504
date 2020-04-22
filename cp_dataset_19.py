821747c854b8cc74c60f45d09d27d9f7c0825378
jaywang-EE <gaaiia37896@gmail.com>
2020-04-22 15:01:17 -0400

    Update cp_dataset_19.py

--- cp_dataset_19.py
+++ cp_dataset_19.py
@@ -118,7 +118,7 @@ class CPDataset(data.Dataset):
         ppant = torch.from_numpy(parse_pants) # [0,1]
 
         # upper cloth
-        print(parse_arrays.shape, parse_cloth.shape, pcm.shape)
+        print(parse_array.shape, parse_cloth.shape, pcm.shape)
         im_c = im * pcm + (1 - pcm) # [-1,1], fill 1 for other parts
         im_h = im * phead - (1 - phead) # [-1,1], fill 0 for other parts
 
